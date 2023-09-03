513195f0b14e445bd462fb572d65b89fc0f5a10e
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-03 15:56:46 +0100

    Did activity 5

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,13 @@ function App(): JSX.Element {
 
 			<main>
 				<div className='cards__wrapper'>
-				{cats.map(cat => <CatCard />)} </div>
+				{cats.map( cat => (<CatCard 
+					name={cat.name}
+					species={cat.species}
+					favFoods={cat.favFoods}
+					birthYear={cat.birthYear}
+				
+				/>))} </div>
 			</main>
 
 			<Footer />
